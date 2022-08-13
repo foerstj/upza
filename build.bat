7b44a50958d219eb1036adff78f5d744553fac79
Johannes Förstner <johannes@famfoerstner.de>
2022-08-13 17:44:16 +0200

    Add my usual batch scripts for building & running the map

---
+++ build.bat
@@ -0,0 +1,27 @@
+:: This script is supposed to be executed from your DS installation folder.
+:: TankCreator and gaspy are expected to be in sibling dirs.
+
+:: name of map
+set map=utraean-peninsula
+:: name of map, case-sensitive
+set map_cs=Utraean Peninsula
+:: path of DSLOA documents dir (where Bits are)
+set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
+:: path of DS installation
+set ds=.
+:: path of TankCreator
+set tc=..\TankCreator
+:: path of GasPy
+set gaspy=..\gaspy
+
+:: Compile map file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
+pushd %gaspy%
+venv\Scripts\python -m build.start_positions_required_levels %map% "%tmp%\Bits"
+popd
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "GPG 2002" -title "%map_cs%" -author "Johannes Förstner"
+if %errorlevel% neq 0 pause
+
+:: Cleanup
+rmdir /S /Q "%tmp%\Bits"
---
+++ run.bat
@@ -0,0 +1,22 @@
+:: This script is supposed to be executed from your DS installation folder.
+:: TankCreator is expected to be in a sibling dir.
+
+:: name of map
+set map=utraean-peninsula
+:: path of DSLOA documents dir (where Bits are)
+set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
+:: path of DS installation
+set ds=.
+:: path of TankCreator
+set tc=..\TankCreator
+
+:: Compile dsmap file
+call "%doc_dsloa%\Bits\build.bat"
+
+::pause
+
+:: Run it!
+"%ds%\DSLOA.exe" nointro=true map=%map%
+
+:: Cleanup resources so as not to confuse Siege Editor
+call "%doc_dsloa%\Bits\cleanup.bat"
