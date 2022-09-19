8522a647f1f981151d120a70755417f9e58ef122
Johannes Förstner <johannes@famfoerstner.de>
2022-09-19 21:53:41 +0200

    Fix error handling in build script

--- build.bat
+++ build.bat
@@ -24,9 +24,10 @@ robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
 pushd %gaspy%
 venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
 if %errorlevel% neq 0 pause
+SETLOCAL EnableDelayedExpansion
 if not "%mode%"=="light" (
   venv\Scripts\python -m build.add_world_levels %map% "%tmp%\Bits" "%doc_dsloa%\Bits"
-  if %errorlevel% neq 0 pause
+  if !errorlevel! neq 0 pause
 )
 popd
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "GPG 2002" -title "%map_cs%" -author "Johannes Förstner"
@@ -40,11 +41,12 @@ robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%map%" "%tmp%\Bits\world\co
 if %errorlevel% neq 0 pause
 
 :: Compile German language resource file
+SETLOCAL EnableDelayedExpansion
 if not "%mode%"=="light" (
   rmdir /S /Q "%tmp%\Bits"
   robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" /E
   %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%.de.dsres" -copyright "CC-BY-SA 2022" -title "%map_cs%" -author "Johannes Förstner"
-  if %errorlevel% neq 0 pause
+  if !errorlevel! neq 0 pause
 )
 
 :: Cleanup
