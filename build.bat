2cb762e4af5e2c9b88a92e7ca22b8e0b6f733bf5
Johannes Förstner <johannes@famfoerstner.de>
2023-07-08 14:05:33 +0200

    quicktest

--- build.bat
+++ build.bat
@@ -51,7 +51,7 @@ if not "%mode%"=="light" (
 )
 if "%mode%"=="light" (
   robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /xf .gitignore /xd regions
-  for %%r in (des_r1 des_r2 dragon dl_r1 dragon_r2 grs2des castle_town desert gp_r1 gp_r2) do (
+  for %%r in (desert oasis oasis_cave eg_seck_dungeon dunes dunes2) do (
     robocopy "%doc_dsloa%\Bits\world\maps\%map%\regions\%%r" "%tmp%\Bits\world\maps\%map%\regions\%%r" /E
   )
 )
